a38a22419f87626526c8c71d59e4309d4507c81d
wb.zkx <wb.zkx@mesg.corp.netease.com>
2024-08-02 17:39:27 +0800

    feat: 添加获取截图

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'game-scripts-controller.oss-cn-qingdao.aliyuncs.com',
+        port: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
